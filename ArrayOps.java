535320caf00876220650dd0b5cebc56e059eee18
NoraArcher <narcher20@stuy.edu>
2020-10-22 16:12:31 -0400

    4e finished!

---
+++ ArrayOps.java
@@ -0,0 +1,45 @@
+public class ArrayOps {
+
+  public static int sum(int[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+  public static int largest(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+
+  public static int[] sumRows(int[][] matrix) {
+    int[] arr = new int[matrix.length];
+    for (int row = 0; row < matrix.length; row++) {
+      arr[row] = sum(matrix[row]);
+    }
+    return arr;
+  }
+
+  public static int[] largestInRows(int[][] matrix) {
+    int[] arr = new int[matrix.length];
+    for (int row = 0; row < matrix.length; row++) {
+      arr[row] = largest(matrix[row]);
+    }
+    return arr;
+  }
+
+  public static int sum(int[][] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += sum(arr[i]);
+    }
+    return sum;
+  }
+
+}
---
+++ Tester.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Tester {
+  // for test cases and print statements
+  public static void main(String[] args) {
+    int[] arrempty = {};
+    int [] arr1 = {1, 9, 4};
+    int[][] deep1 = {{4, 5, 1}, {3, 6, 9}, {17, 45, 7}, {12, 12, 13}};
+
+    System.out.println(ArrayOps.sum(arr1));
+    System.out.println(ArrayOps.sum(arrempty));
+
+    System.out.println(ArrayOps.largest(arr1));
+
+    System.out.println(Arrays.toString(ArrayOps.sumRows(deep1)));
+
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(deep1)));
+
+    System.out.println(ArrayOps.sum(deep1));
+  }
+
+}
