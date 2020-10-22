fdf89519a957f122eaa6e8373318951c607d06e8
NoraArcher <narcher20@stuy.edu>
2020-10-22 16:22:00 -0400

    4b finished, not sure if my test cases test a wide enough range

--- ArrayOps.java
+++ ArrayOps.java
@@ -18,28 +18,4 @@ public class ArrayOps {
     return max;
   }
 
-  public static int[] sumRows(int[][] matrix) {
-    int[] arr = new int[matrix.length];
-    for (int row = 0; row < matrix.length; row++) {
-      arr[row] = sum(matrix[row]);
-    }
-    return arr;
-  }
-
-  public static int[] largestInRows(int[][] matrix) {
-    int[] arr = new int[matrix.length];
-    for (int row = 0; row < matrix.length; row++) {
-      arr[row] = largest(matrix[row]);
-    }
-    return arr;
-  }
-
-  public static int sum(int[][] arr) {
-    int sum = 0;
-    for (int i = 0; i < arr.length; i++) {
-      sum += sum(arr[i]);
-    }
-    return sum;
-  }
-
 }
